ac1a475eb337a8961652b58f9f4bf8a392ff800e
Kyle Hyer <krhyer@gmail.com>
2017-06-27 11:38:44 -0600

    adds config for heroku deploymnet

--- server/config/dev-server.js
+++ server/config/dev-server.js
@@ -31,6 +31,7 @@ function logger(req, res, next) {
 app.use(session)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '../public'))
 app.use('*', logger)
 app.use('*', cors(corsOptions))
 app.use('/', Auth)
