17000861d87d1cbc1ef81f238b05f42410e819ca
Kyle Hyer <krhyer@gmail.com>
2017-06-30 09:21:34 -0600

    deploy to heroku

--- www/src/store/index.js
+++ www/src/store/index.js
@@ -6,12 +6,12 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 let api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'http://keepr-proj.herokuapp.com/api/',
   timeout: 2000,
   withCredentials: true
 })
 let auth = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://keepr-proj.herokuapp.com/',
   timeout: 2000,
   withCredentials: true
 })
